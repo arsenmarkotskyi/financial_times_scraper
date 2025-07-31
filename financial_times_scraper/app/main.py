8c6e6e607878281236748a5afb4f644c0bed53d0
Arsen Markotskyi <arsen.markotskui@gmail.com>
2025-07-31 13:52:33 +0300

    add scraper to main

--- financial_times_scraper/app/main.py
+++ financial_times_scraper/app/main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
 
+from financial_times_scraper.app.scraper import scrape_ft
+
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello, Financial Times!"}
+
+
+
+if __name__ == "__main__":
+    import asyncio
+
+
+    async def periodic_scrape():
+        """
+        Запускає нескінченний цикл скрапінгу з інтервалом в 1 годину.
+        """
+        while True:
+            print("Starting scrape cycle...")
+            await scrape_ft()
+            print("Scrape cycle done. Sleeping 1 hour...")
+            await asyncio.sleep(3600)
+
+
+    asyncio.run(periodic_scrape())
